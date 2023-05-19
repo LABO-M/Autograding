cbe8a25d03c02c4b88fbaf59a266312149da9ab3
ShoSampo <poohsan.pilot79@gmail.com>
2023-05-19 15:42:11 +0900

    複数ファイルの採点処理の対応と学生名の取得方法の変更

--- notebook.py
+++ notebook.py
@@ -1,3 +1,4 @@
+import re
 import json
 import pathlib
 from typing import Dict, List
@@ -8,6 +9,28 @@ class StudentNotebook:
         self.student_path = student_path
         self.model_ans = model_ans
         self.cells = load_cells(self.student_path)
+        self.student_name = self.get_student_name()
+
+    def get_student_name(self) -> str:
+        return self.student_path.parent.parts[-1].split("_")[0]
+
+    def get_student_info(self):
+        # 学籍番号と氏名を取得するセルを特定
+        info_cell_index = identify_cell(sentence="### 学籍番号、氏名を入力せよ\n", cells=self.cells, cell_type='markdown')
+
+        if info_cell_index != -1:
+            info_cell = self.cells[info_cell_index]
+            info_text = "\n".join(info_cell['source'])
+
+            # 学籍番号と氏名を抽出
+            student_id = re.search(r'学籍番号：\s*([\w-]+)', info_text.replace('\n', ' '))
+            student_name = re.search(r'氏名:\s*([\w\s]+)', info_text.replace('\n', ' '))
+
+            if student_id and student_name:
+                return student_id.group(1), student_name.group(1).replace(' ', '')
+
+        # 学籍番号と氏名が見つからない場合
+        return None, None
 
     def grade(self) -> int:
         score = 0
