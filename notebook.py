eaf7b6da7c7f74dfcd17175e84103f7c771412ca
ShoSampo <poohsan.pilot79@gmail.com>
2023-05-19 15:44:52 +0900

    notebookから学生氏名と学籍番号を取得するメソッドの削除

--- notebook.py
+++ notebook.py
@@ -14,24 +14,6 @@ class StudentNotebook:
     def get_student_name(self) -> str:
         return self.student_path.parent.parts[-1].split("_")[0]
 
-    def get_student_info(self):
-        # 学籍番号と氏名を取得するセルを特定
-        info_cell_index = identify_cell(sentence="### 学籍番号、氏名を入力せよ\n", cells=self.cells, cell_type='markdown')
-
-        if info_cell_index != -1:
-            info_cell = self.cells[info_cell_index]
-            info_text = "\n".join(info_cell['source'])
-
-            # 学籍番号と氏名を抽出
-            student_id = re.search(r'学籍番号：\s*([\w-]+)', info_text.replace('\n', ' '))
-            student_name = re.search(r'氏名:\s*([\w\s]+)', info_text.replace('\n', ' '))
-
-            if student_id and student_name:
-                return student_id.group(1), student_name.group(1).replace(' ', '')
-
-        # 学籍番号と氏名が見つからない場合
-        return None, None
-
     def grade(self) -> int:
         score = 0
         question_cells = get_question_cells(self.cells)
